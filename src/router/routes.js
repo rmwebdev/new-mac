80ef851f0373c4e258ebce966f71ea11cf31199e
tog.hiero <hieronabit02@gmail.com>
2022-10-28 15:02:23 +0700

    fix route hash

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,31 @@
 const routes = [
   {
-    path: "/",
+    path:"/vending-machine",
+    vueRouterMode: 'history',
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/HomePage.vue") }],
+    children: [
+      {
+        path: "",
+        name: "HomePage",
+        component: () => import("pages/HomePage.vue"),
+      },
+    ]
+
   },
   {
-    path: "/conversion",
+    path:"/vending-machines",
+    vueRouterMode: 'history',
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/HomePageCoversion.vue") }],
+    children: [
+      {
+        path: "",
+        name: "PageConversion",
+        component: () => import("pages/PageCoversion.vue"),
+      },
+    ]
+
   },
+
 
   // Always leave this as last one,
   // but you can also remove it
